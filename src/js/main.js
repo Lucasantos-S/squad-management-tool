118ba9f8a44711e2515dc3ad6ecf6341f38a3a39
lucasantos-S <lucassantos.dsilv@gmail.com>
2022-07-31 12:45:38 -0300

    create your team modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
+/* ativando o modal apos o click no botão*/
+const buttonModal = document.querySelector(".btn-modal");
+const modalContainer = document.querySelector(".modal-container");
+const btnCrud = document.querySelectorAll(".btn-crud");
 
+/* apos clicar e adicionado a class active no modal-container*/
+const activeModal = () => modalContainer.classList.add("active");
 
+/* um if verificando se o clique esta sendo no modal container*/
+const removeActive = (event) =>
+  modalContainer === event.target
+    ? modalContainer.classList.remove("active")
+    : null;
+
+buttonModal.addEventListener("click", activeModal);
+
+modalContainer.addEventListener("click", removeActive);
