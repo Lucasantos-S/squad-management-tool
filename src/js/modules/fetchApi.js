05b7b3e6800f64c80587caa5832493bdb5b796dd
lucasantos-S <lucassantos.dsilv@gmail.com>
2022-08-06 14:19:53 -0300

    refactored Debounce

--- src/js/modules/fetchApi.js
+++ src/js/modules/fetchApi.js
@@ -1,20 +1,22 @@
 import { responsePlay } from "./playerField.js";
-let time = null;
 
 const getPlayerApi = async (name) =>
   fetch(
     `http://api.squadmanagementtoll.kinghost.net/wp-json/api/jogadores?q=${name}`
   ).then((res) => res.json());
 
-function debounceEvent(value) {
-  clearTimeout(time);
-  time = setTimeout(() => {
-    getPlayerApi(value).then((player) => responsePlay(player))
-  }, 1000);
+function debounceEvent() {
+  let time = null;
+  return function (value) {
+    clearTimeout(time);
+    time = setTimeout(() => {
+      getPlayerApi(value).then((player) => responsePlay(player));
+    }, 1000);
+  };
 }
-const handleKeyUp = ({ target }) => {
-  debounceEvent(target.value);
-};
+
+const debounce = debounceEvent();
+const handleKeyUp = ({ target }) => debounce(target.value);
 
 document
   .getElementById("search-Players")
